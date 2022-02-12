5383cc85d0b8b63338508bf134ccc08e67c79e88
Payne <wuzhipeng1289690157@gmail.com>
2022-02-12 21:09:36 +0800

    init

---
+++ ss.js
@@ -0,0 +1,50 @@
+window = global;
+var OOOOOO = ["dG9TdHJpbmc=", "MA==", "Z2V0RGF0ZQ==", "bG9n", "eXl5eS1NTS1kZA==", "RGF0ZQ==", "cHJvdG90eXBl", "Zm9ybWF0", "cmVwbGFjZQ==", "Z2V0RnVsbFllYXI=", "Z2V0TW9udGg="];
+
+(function (OOOOOO, OOOOOo) {
+  var OOOOO0 = function (OOOOOo) {
+    while (--OOOOOo) {
+      OOOOOO["\x70\x75\x73\x68"](OOOOOO["\x73\x68\x69\x66\x74"]());
+    }
+  };
+
+  OOOOO0(++OOOOOo);
+})(OOOOOO, 805473 ^ 805489);
+
+window[atob(OOOOOO[857521 ^ 857521])][atob(OOOOOO[800845 ^ 800844])][atob(OOOOOO[160297 ^ 160299])] = function (OOOOOo) {
+  function OOOoOO(OOOOOO, OOOOOo) {
+    return OOOOOO + OOOOOo;
+  }
+
+  function OOOO00(OOOOOO, OOOOOo) {
+    return OOOOOO > OOOOOo;
+  }
+
+  function OOOO0o(OOOOOO, OOOOOo) {
+    return OOOOOO + OOOOOo;
+  }
+
+  function OOOO0O(OOOOOO, OOOOOo) {
+    return OOOOOO + OOOOOo;
+  }
+
+  function OOOOo0(OOOOOO, OOOOOo) {
+    return OOOOOO + OOOOOo;
+  }
+
+  function OOOOoo(OOOOOO, OOOOOo) {
+    return OOOOOO + OOOOOo;
+  }
+
+  function OOOOoO(OOOOOO, OOOOOo) {
+    return OOOOOO > OOOOOo;
+  }
+
+  var OOOOO0 = OOOOOo;
+  OOOOO0 = OOOOO0[atob(OOOOOO[539048 ^ 539051])](/yyyy|YYYY/, this[atob(OOOOOO[111224 ^ 111228])]());
+  OOOOO0 = OOOOO0[atob(OOOOOO[899020 ^ 899023])](/MM/, OOOOoO(OOOOoo(this[atob(OOOOOO[751247 ^ 751242])](), 734611 ^ 734610), 384514 ^ 384523) ? OOOOo0(this[atob(OOOOOO[772257 ^ 772260])](), 529031 ^ 529030)[atob(OOOOOO[543844 ^ 543842])]() : OOOO0O(atob(OOOOOO[516654 ^ 516649]), OOOO0o(this[atob(OOOOOO[544368 ^ 544373])](), 441819 ^ 441818)));
+  OOOOO0 = OOOOO0[atob(OOOOOO[292147 ^ 292144])](/dd|DD/, OOOO00(this[atob(OOOOOO[928236 ^ 928228])](), 312225 ^ 312232) ? this[atob(OOOOOO[769321 ^ 769313])]()[atob(OOOOOO[573151 ^ 573145])]() : OOOoOO(atob(OOOOOO[609576 ^ 609583]), this[atob(OOOOOO[602961 ^ 602969])]()));
+  return OOOOO0;
+};
+
+console[atob(OOOOOO[749040 ^ 749049])](new window[atob(OOOOOO[871677 ^ 871677])]()[atob(OOOOOO[931389 ^ 931391])](atob(OOOOOO[200506 ^ 200496])));
